26389b8ea5150c7f9b28b61817a2c1fb57e1d86c
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-28 21:52:36 -0300

    feat: create errorHandlerMiddleware.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(errorHandlerMiddleware);
 
 export default app
