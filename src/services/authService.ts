384957703bff93160ca8f323bd0d76725a433007
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-29 01:14:48 -0300

    feat: create generateToken - authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import { findUserByEmail, insertUser, TCreateUser } from '../repositories/authRepository';
+dotenv.config();
 
 export async function createUser(user: TCreateUser) {
     await isUniqueEmail(user.email);
@@ -13,4 +16,17 @@ export async function isUniqueEmail(email: string) {
     if(user) {
         throw { type: "conflict", message: 'Email já cadastrado' };
     }
+}
+
+export function generateToken(id: number) {
+    const secret = process.env.TOKEN_SECRET_KEY;
+
+    if(!secret) {
+        throw { type: "code", message: '.env não implementado' }
+    }
+
+    const expiresIn = process.env.EXPIRES_IN;
+    const token = jwt.sign({ id }, secret, { expiresIn });
+
+    return token
 }
