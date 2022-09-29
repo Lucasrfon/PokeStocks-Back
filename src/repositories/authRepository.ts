4754729b06af0af05ebe6715a1021fc310c81ab8
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-29 00:58:10 -0300

    feat: create insertUser - authRepository

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,5 +1,12 @@
+import { Users } from "@prisma/client";
 import prisma from "../dbStrategy/prisma";
+
+export type TCreateUser = Omit<Users, 'id'>;
 
 export async function findUserByEmail(email: string) {
     return await prisma.users.findFirst({where: { email }});
+}
+
+export async function insertUser(user: TCreateUser) {
+    await prisma.users.create({data: user});
 }
