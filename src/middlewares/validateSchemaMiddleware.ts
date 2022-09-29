39c9f4feb7ee3767f3440d7ec4abfb41a1f3d37f
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-28 22:50:42 -0300

    feat: create validateSchemaMiddleware

---
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import joi from "joi";
+
+export default function validateSchema(schema: joi.ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => { 
+    const {error} = schema.validate(req.body, {abortEarly: false});
+        
+    if (error) {
+      throw { type: "schema", message: error.details.map(detail => detail.message) }
+    }
+    next();
+  }
+}
