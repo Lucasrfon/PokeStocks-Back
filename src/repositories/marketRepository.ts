4d35f09b255244939a9eb237d2f3bdb5014609b7
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-05 20:22:26 -0300

    feat: create getUserWallet marketRepository

--- src/repositories/marketRepository.ts
+++ src/repositories/marketRepository.ts
@@ -3,3 +3,7 @@ import prisma from "../dbStrategy/prisma";
 export async function getStockMarket(email: string) {
   return await prisma.market.findMany();
 }
+
+export async function getUserWallet(id: number) {
+  return await prisma.wallet.findMany();
+}
