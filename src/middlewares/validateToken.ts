983558bcc807f51d6749bd9178454c17af4ea8a8
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-05 15:08:36 -0300

    feat: create validateToken

---
+++ src/middlewares/validateToken.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default function validateToken() {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    const secret = process.env.TOKEN_SECRET_KEY;
+
+    if (!token) {
+      throw { type: "unauthorized"};
+    }
+
+    if (!authorization?.startsWith("Bearer ")) {
+      throw { type: "unauthorized"};
+    }
+
+    if (!secret) {
+      throw { type: "code"};
+    }
+
+    jwt.verify(token, secret);
+    next();
+  };
+}
