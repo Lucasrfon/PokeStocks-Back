acec15de63fa4d6b20bd6cb7299b04b689ca5367
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-07 13:18:26 -0300

    feat: marketController on marketRouter

--- src/routers/marketRouter.ts
+++ src/routers/marketRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { requestBuyStocks, requestMarket, requestSellStocks } from "../controllers/marketController";
 import validateToken from "../middlewares/validateToken";
 
 const marketRouter = Router();
 
 marketRouter.use(validateToken());
-marketRouter.get("/market");
-marketRouter.post("/market/buy");
-marketRouter.post("/market/sell");
+marketRouter.get("/market", requestMarket);
+marketRouter.post("/market/buy", requestBuyStocks);
+marketRouter.post("/market/sell", requestSellStocks);
 
 export default marketRouter;
