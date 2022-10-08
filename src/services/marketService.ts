fd4adedf9aaf62ded63196fe3ebb888b632da7d0
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-07 21:00:49 -0300

    fix: change buyStocks marketService to increment instead upgrading

--- src/services/marketService.ts
+++ src/services/marketService.ts
@@ -19,31 +19,31 @@ export async function buyStocks(buyOrder: TMarket, id: number) {
     throw { type: "not found" };
   }
   const totalValue = buyOrder.amount * pokeMarket.value;
-  const newCreditBalance = user.credit - totalValue;
+  const creditBalance = -totalValue;
 
   if (user.credit < totalValue) {
     throw { type: "transaction denied", message: "not enough credits" };
   }
 
-  await updateCredits(id, newCreditBalance);
+  await updateCredits(id, creditBalance);
   await updateWallet(buyOrder, id);
 }
 
 export async function sellStocks(sellOrder: TMarket, id: number) {
-    const wallet = await getUserWallet(sellOrder.pokemonId, id);
+  const wallet = await getUserWallet(sellOrder.pokemonId, id);
 
-    if(!wallet || wallet.amount < sellOrder.amount) {
-        throw { type: "transaction denied", message: "not enough stocks" };
-    }
-    const pokeMarket = await getStockMarketById(sellOrder.pokemonId);
-  
-    if (!pokeMarket) {
-      throw { type: "not found" };
-    }
-
-    const creditBalance = sellOrder.amount * pokeMarket.value;
-    sellOrder.amount = -sellOrder.amount;
-  
-    await updateCredits(id, creditBalance);
-    await updateWallet(sellOrder, id);
+  if (!wallet || wallet.amount < sellOrder.amount) {
+    throw { type: "transaction denied", message: "not enough stocks" };
   }
+  const pokeMarket = await getStockMarketById(sellOrder.pokemonId);
+
+  if (!pokeMarket) {
+    throw { type: "not found" };
+  }
+
+  const creditBalance = sellOrder.amount * pokeMarket.value;
+  sellOrder.amount = -sellOrder.amount;
+
+  await updateCredits(id, creditBalance);
+  await updateWallet(sellOrder, id);
+}
