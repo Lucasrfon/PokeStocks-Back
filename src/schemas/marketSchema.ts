cda3427903e50d68512c5c10e2664b31da601b6e
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-07 22:06:50 -0300

    fix: add .required() marketSchema

--- src/schemas/marketSchema.ts
+++ src/schemas/marketSchema.ts
@@ -1,8 +1,8 @@
 import joi from "joi";
 
 const marketSchema = joi.object({
-  pokemonId: joi.number().min(1).max(151),
-  amount: joi.number().integer().min(1),
+  pokemonId: joi.number().min(1).max(151).required(),
+  amount: joi.number().integer().min(1).required(),
 });
 
 export { marketSchema };
