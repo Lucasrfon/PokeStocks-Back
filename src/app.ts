cfd0327ec7d5b6ebe831b6aceea8926f138a8f5f
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-16 23:32:41 -0300

    feat: add rollMarket in app

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import cors from "cors";
 import "express-async-errors";
 import router from "./routers/index";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
+import { rollMarket } from "./utils/rollMarket";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use(errorHandlerMiddleware);
+
+rollMarket();
 
 export default app;
